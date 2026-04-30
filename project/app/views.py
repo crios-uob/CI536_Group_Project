d126e804052a46c7f33fb36bf5c63126fda95297
crios-uob <C.rios1@uni.brighton.ac.uk>
2026-04-30 08:37:34 -0400

    fix(explicit naming): Add more explicit naming

--- project/app/views.py
+++ project/app/views.py
@@ -3,18 +3,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def load(request: HttpRequest, html_filename: str, tags: dict[str, str]) -> HttpResponse:
+def test(request: HttpRequest) -> HttpResponse:
     """
     Loads an html file and renders it with the Django template tags. 
 
     Args:
-        request: Http Request Objec
-        html_filename: The filename of the html file to be loaded
-        tags: A dict of tags to showing what each tag should be defined as 
+        request: Http Request Object
 
     Returns:
         HttpResponse Object with rendered HTML 
     """
-    return render(request, html_filename, tags)
+    return render(request, 'test.html', {'name': 'Jason'})
 
 
