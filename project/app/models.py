02fb4b03380fd14d7f481bd9bfcd8bc6360d2892
EmmaO-531 <ueonwubiko@gmail.com>
2026-05-01 13:45:51 +0100

    database models

---
+++ project/app/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Deck(models.Model):
+    name = models.CharField(max_length=100)
+    category= models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+class Card(models.Model):
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
+    question = models.CharField(max_length=255)
+    answer = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.question
+    
+class Result(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
+    score = models.IntegerField()
+    date_taken = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.deck.name} - {self.score}"
