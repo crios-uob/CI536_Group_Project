76dcc5682462659c3ad463726d6d94244b51f8f3
Neve <neveleachh@gmail.com>
2026-05-15 05:05:21 +0100

    user login test case

--- project/app/tests.py
+++ project/app/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from app.views import analytics
 
-# Create your tests here.
+#views.py tests
+class userloginTestCase(TestCase):
+    def login(self): #creates a test instance of model
+        response = self.client.get('/analytics')
+        self.assertEqual(response.status_code, 302) #checking if equal
+
+    def useraccess(self):
+        user = User.objects.create_user(
+            username='testuser',
+            password='password'
+        )
+
+        self.client.login(
+            username='testuser',
+            password='password123'
+        )
+
+        response = self.client.get('/analytics')
+        self.assertEqual(response.status_code, 200)
